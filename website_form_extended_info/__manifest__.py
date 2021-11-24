9e16e8e440177ad429ad5af4a3b1821f830d0b20
Krys <krys@railabo.com>
2021-11-25 00:14:16 +0100

    Basic structure and fucntionality - phase 1.0.1.16

--- website_form_extended_info/__manifest__.py
+++ website_form_extended_info/__manifest__.py
@@ -8,6 +8,8 @@
     "license": "LGPL-3",
     "version": "15.0.1.0.1",
     "depends": ["website"],
+    "images": ["static/description/images/cover.png",
+               "static/description/images/cover_screenshot.png"],
     "installable": True,
     "auto_install": False,
     "application": False,
